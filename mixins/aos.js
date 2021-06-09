e46164f61081683a255c7478b6851dde23f6908e
Matej <matej.kapular@gauss.hr>
2021-06-09 16:06:08 +0200

    fix: test fix for aos 2

--- mixins/aos.js
+++ mixins/aos.js
@@ -2,12 +2,7 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 export default {
-  mounted() {
-    AOS.init({
-      disable: function () {
-        const maxWidth = 1024
-        return window.innerWidth < maxWidth
-      },
-    })
+  async mounted() {
+    import('aos').then((AOS) => AOS.init())
   },
 }
