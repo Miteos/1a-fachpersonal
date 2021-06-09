29a47c25a4f6e2b00e4d00031549083e273c0f35
Matej <matej.kapular@gauss.hr>
2021-06-09 15:29:10 +0200

    fix: random typo

--- components/sections/landingCarousel.vue
+++ components/sections/landingCarousel.vue
@@ -31,7 +31,7 @@ export default {
         'deep-purple accent-4',
       ],
       slides: [
-        'Personalberatung und Headhunt aus einer Hand  1A -Fachpersonal.',
+        'Personalberatung und Headhunt aus einer Hand      -> 1A-Fachpersonal.',
         ' Dank eines nahezu perfekten Executive Search Instrumentariums sowie herausragender Managementdiagnostiken und Organisationsdiagnostiken findet die 1A - Fachpersonal für Sie die passenden Führungskräfte und Manager.',
         'Dabei profitiert Ihr Unternehmen von der Beratungskompetenz und Branchenerfahrung.',
         'Ihre Daten sind bei uns sicher. Ihre Daten werden auf\n' +
